c45f5816686ef4da588534fe0fc397f3e8944871
Eric Hartley <spiffydude0@gmail.com>
2024-09-17 17:57:57 -0500

    Exercise completed

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+        
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+        public double GetBalance() 
+        { 
+            return _balance; 
+        }
+        public void Withdraw(double amount)
+        {
+            _balance -= amount;
+        }
+        
+        
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,87 @@
-﻿namespace BankEncapsulation
+﻿using System.ComponentModel.Design;
+using System.Security.Principal;
+
+namespace BankEncapsulation
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+
+            ATM(account);
+        }
+
+        private static BankAccount ATM(BankAccount account)
+        {
+            Console.WriteLine("Welcome! What can I help you with today?");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Balance Inquiry");
+            Console.WriteLine("3. Withdraw");
+            Console.WriteLine("0. Exit");
+
+            var input = Console.ReadLine();
+
+            if (int.TryParse(input, out int option))
+            {
+                if (option == 1)
+                {
+                    Console.WriteLine("How much would you like to deposit?");
+                    double.TryParse(Console.ReadLine(), out double amount);
+
+                    if (amount > 0.00)
+                    {
+                        account.Deposit(amount);
+                        Console.WriteLine($"Deposit of ${amount} complete.");
+                        return ATM(account);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Try again.");
+                        return ATM(account);
+                    }
+
+                }
+                else if (option == 2)
+                {
+                    Console.WriteLine($"Your balance is ${account.GetBalance()}");
+                    return ATM(account);
+                }
+                else if (option == 3)
+                {
+                    Console.WriteLine("How much would you like to withdraw?");
+                    double.TryParse(Console.ReadLine(), out double amount);
+
+                    if (amount > 0.00 && amount < account.GetBalance())
+                    {
+                        account.Withdraw(amount);
+                        Console.WriteLine($"withdrawal of ${amount} complete.");
+                        return ATM(account);
+
+                    }
+                    else if (amount > 0.00 && amount > account.GetBalance())
+                    {
+                        Console.WriteLine("You do not have the funds to complete" +
+                            " this withdrawal.");
+                        return ATM(account);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Try again.");
+                        return ATM(account);
+                    }
+                }
+                else if (option == 0)
+                {
+                    Console.WriteLine("Have a great day.");
+                    return account;
+                }
+                Console.WriteLine("Invalid Input");
+                return ATM(account);
+            }
+            
+            return account;
         }
     }
 }
